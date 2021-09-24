a055dceecd401e468e422107f182ca9450cf55cd
Mnav02 <91284114+Mnav02@users.noreply.github.com>
2021-09-24 10:19:12 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    . . . . . . . f f f f f . . . . 
+    . . . . . . f a a a a a f . . . 
+    . . . . . f a a f f f f f f . . 
+    . . f f f f a a f d d 1 1 f . . 
+    . f a c a f a f d 1 1 1 1 1 f . 
+    f a c a a f a f d 1 1 1 1 1 f . 
+    f c a a a f a a f 1 1 1 1 f . . 
+    f a a a a f a a f f f f f f . . 
+    f a a a a f a a a a a a a f . . 
+    f a a a a f a a a a a a a f . . 
+    . f a a a f a a a a a a a f . . 
+    . . f f f f a a a a a a a f . . 
+    . . . . . f a a a f a a a f . . 
+    . . . . . f a a a f a a a f . . 
+    . . . . . f a a a f a a a f . . 
+    . . . . . f f f f f f f f f . . 
+    `, SpriteKind.Player)
